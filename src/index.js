7d2b2406f995d2ed1c866f11769aa51422b9ae89
James Tan <xiang.tan.p2109@gmail.com>
2021-03-31 11:53:59 +1030

    add read preference in mongo uri

--- src/index.js
+++ src/index.js
@@ -9,61 +9,55 @@ console.log(readUrl);
 
 (async () => {
   try {
-    await mongoose.connect(`mongodb://${readUrl}/users`, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
- 
+    await mongoose.connect(
+      `mongodb://${readUrl}/users?readPreference=secondary`,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      }
+    );
 
     const query = { age: { $gt: 22 } };
 
     console.time('query');
-    await User.find(query).read('secondaryPreferred');
+    await User.find(query);
     console.timeEnd('query');
 
     console.time('query_with_index');
-    await UserWithIndex.find(query).read('secondaryPreferred');
+    await UserWithIndex.find(query);
     console.timeEnd('query_with_index');
 
     console.time('query_with_select');
-    await User.find(query)
-      .select({ name: 1, _id: 1, age: 1, email: 1 })
-      .read('secondaryPreferred');
+    await User.find(query).select({ name: 1, _id: 1, age: 1, email: 1 });
     console.timeEnd('query_with_select');
 
     console.time('query_with_select_index');
-    await UserWithIndex.find(query)
-      .select({
-        name: 1,
-        _id: 1,
-        age: 1,
-        email: 1,
-      })
-      .read('secondaryPreferred');
+    await UserWithIndex.find(query).select({
+      name: 1,
+      _id: 1,
+      age: 1,
+      email: 1,
+    });
     console.timeEnd('query_with_select_index');
 
     console.time('lean_query');
-    await User.find(query).lean().read('secondaryPreferred');
+    await User.find(query).lean();
     console.timeEnd('lean_query');
 
     console.time('lean_with_index');
-    await UserWithIndex.find(query).lean().read('secondaryPreferred');
+    await UserWithIndex.find(query).lean();
     console.timeEnd('lean_with_index');
 
     console.time('lean_with_select');
-    await User.find(query)
-      .select({ name: 1, _id: 1, age: 1, email: 1 })
-      .lean()
-      .read('secondaryPreferred');
+    await User.find(query).select({ name: 1, _id: 1, age: 1, email: 1 }).lean();
     console.timeEnd('lean_with_select');
 
     console.time('lean_select_index');
     await UserWithIndex.find(query)
       .select({ name: 1, _id: 1, age: 1, email: 1 })
-      .lean()
-      .read('secondaryPreferred');
+      .lean();
     console.timeEnd('lean_select_index');
     process.exit(0);
   } catch (err) {
